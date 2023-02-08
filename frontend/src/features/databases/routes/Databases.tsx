d6996facbe7924a22c81b3275e8a31192cb82f70
Seunghyun Yu <yshyun123@gmail.com>
2023-02-09 01:29:21 +0900

    feat: Add CreateDatabse UI

--- frontend/src/features/databases/routes/Databases.tsx
+++ frontend/src/features/databases/routes/Databases.tsx
@@ -1,10 +1,12 @@
 import { ContentLayout } from '@/components/Layout';
 
+import CreateDatabase from '../components/CreateDatabase';
 import DatabasesList from '../components/DatabasesList';
 
 export default function Databases() {
 	return (
 		<ContentLayout title="Databases">
+			<CreateDatabase />
 			<DatabasesList />
 		</ContentLayout>
 	);

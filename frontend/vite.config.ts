81b0e39bc2744bf93f1256fc1b63fe131a6995c2
Seunghyun Yu <yshyun123@gmail.com>
2023-03-08 01:18:32 +0900

    feat: set front-end's proxy setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
-  },
-  plugins: [react()],
-})
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		proxy: {
+			'/v1': {
+				target: 'http://3.92.239.238:32600',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	plugins: [react()],
+});
